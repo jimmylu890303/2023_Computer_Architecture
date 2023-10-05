b851d334df8c7e3f31ca036f6317232d41c709c0
jimmylu890303 <jimmylu9460@gmail.com>
2023-10-05 20:47:45 +0800

    Doing float32 mul

--- assignment/hw1/main.s
+++ assignment/hw1/main.s
@@ -24,12 +24,6 @@ bf16mul:     .string "The value of BFloat16 mul is "
 # /*    main    */
 main:
     
-    # test function
-    jal ra, test
-    la a0, nextLine
-    li a7, 4
-    ecall
-    
     # float32 mul
     la a0, fp32mul
     li a7, 4
@@ -40,27 +34,6 @@ main:
     li a7,34             # 0xc5664300 = -3684.1875
     ecall
     la a0, nextLine
-    li a7, 4
-    ecall 
-    
-    # Convert same valuse from fp32 to bf16
-    li a0, 0x42488000    # 50.125
-    jal fp32_to_bf16
-    mv s0, a0
-    li a0, 0xc2930000    # -73.5
-    jal fp32_to_bf16
-    mv s1, a0
-    
-    # bfloat16 mul
-    la a0, bf16mul
-    li a7, 4
-    ecall 
-    mv a0, s0    # 50.125
-    mv a1, s1    # -73.5
-    jal bfloat16_mul
-    li a7,34
-    ecall
-    la a0, nextLine      # 0xc5660000 = -3680.0
     li a7, 4
     ecall 
     
